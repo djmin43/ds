3d54c27c1712f27c579cb58b1e09ec1f0d773d0f
jay <dj.min43@gmail.com>
2022-04-10 17:43:53 +0900

    add bunch of button components

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Meta } from "@storybook/react/types-7-0";
+import Button, { ButtonProps } from "./Button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+} as Meta;
